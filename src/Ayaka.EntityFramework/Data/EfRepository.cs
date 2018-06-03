161166d8a1576bdba1ddc024463e2b8956a3bee9
Raphael Strotz <Xzelsius@users.noreply.github.com>
2018-06-03 20:45:03 +0200

    Added EntityFramework implementation for IRepository and IAsynRepository

---
+++ src/Ayaka.EntityFramework/Data/EfRepository.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) Raphael Strotz and other contributors under the MIT license. All rights reserved.
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ayaka.Data
+{
+    /// <summary>
+    ///     Provides functionality to interact with data source objects using EntityFramework.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    public class EfRepository<TEntity> : IAsyncRepository<TEntity>, IRepository<TEntity> where TEntity : class, IEntity
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        /// <summary>
+        ///     Initializes a new instance of <see cref="EfRepository{TEntity}" /> class.
+        /// </summary>
+        /// <param name="dbContext">The underlying data context.</param>
+        public EfRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        /// <summary>
+        ///     Provides functionality to evaluate queries against the underlying data source.
+        /// </summary>
+        /// <value>
+        ///     The <see cref="IQueryable{T}" /> to evaluate queries against the underlying data source.
+        /// </value>
+        public virtual IQueryable<TEntity> Table => _dbSet;
+
+        /// <summary>
+        ///     Finds an entity by its primary key.
+        /// </summary>
+        /// <param name="id">The identifier of the entity.</param>
+        /// <returns>The <typeparamref name="TEntity" /> found for the specified primary key.</returns>
+        public virtual TEntity Find(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        /// <summary>
+        ///     Finds an entity by its primary key asynchronously.
+        /// </summary>
+        /// <param name="id">The identifier of the entity.</param>
+        /// <returns>The <typeparamref name="TEntity" /> found for the specified primary key.</returns>
+        public virtual Task<TEntity> FindAsync(int id)
+        {
+            return _dbSet.FindAsync(id);
+        }
+
+        /// <summary>
+        ///     Inserts the entity into the database
+        /// </summary>
+        /// <param name="entity">The entity to insert.</param>
+        public virtual void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        ///     Inserts the entity into the data source asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to insert.</param>
+        /// <returns>A task that represents the asynchronous save operation.</returns>
+        public virtual Task InsertAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            return _dbContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        ///     Persists the modification on the entity to the database.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        public virtual void Update(TEntity entity)
+        {
+            if (_dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+                _dbContext.Entry(entity).State = EntityState.Modified;
+            }
+
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        ///     Persists the modification on the entity to the data source asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        /// <returns>A task that represents the asynchronous save operation.</returns>
+        public virtual Task UpdateAsync(TEntity entity)
+        {
+            if (_dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+                _dbContext.Entry(entity).State = EntityState.Modified;
+            }
+
+            return _dbContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        ///     Deletes the entity from the database.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        public virtual void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        ///     Deletes the entity from the data source asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        /// <returns>A task that represents the asynchronous save operation.</returns>
+        public virtual Task DeleteAsync(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            return _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ test/Ayaka.EntityFramework.Tests/Data/EfRepositoryTests.cs
@@ -0,0 +1,273 @@
+﻿// Copyright (c) Raphael Strotz and other contributors under the MIT license. All rights reserved.
+
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Xunit;
+
+namespace Ayaka.Data
+{
+    public class EfRepositoryTests
+    {
+        [Fact]
+        public void Can_find_entity()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_find_entity")
+                .Options;
+
+            int id;
+            using (var context = new TestContext(options))
+            {
+                context.Set<TestEntity>().Add(new TestEntity {Name = "Test"});
+                context.SaveChanges();
+
+                id = context.Set<TestEntity>().Single().Id;
+            }
+
+            using (var context = new TestContext(options))
+            {
+                var repository = new EfRepository<TestEntity>(context);
+                var entity = repository.Find(id);
+
+                Assert.NotNull(entity);
+                Assert.Equal("Test", entity.Name);
+            }
+        }
+
+        [Fact]
+        public async void Can_find_entity_async()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_find_entity_async")
+                .Options;
+
+            int id;
+            using (var context = new TestContext(options))
+            {
+                context.Set<TestEntity>().Add(new TestEntity {Name = "Test"});
+                await context.SaveChangesAsync();
+
+                id = context.Set<TestEntity>().Single().Id;
+            }
+
+            using (var context = new TestContext(options))
+            {
+                var repository = new EfRepository<TestEntity>(context);
+                var entity = await repository.FindAsync(id);
+
+                Assert.NotNull(entity);
+                Assert.Equal("Test", entity.Name);
+            }
+        }
+
+        [Fact]
+        public void Can_insert_entity()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_insert_entity")
+                .Options;
+
+            using (var context = new TestContext(options))
+            {
+                var repository = new EfRepository<TestEntity>(context);
+                repository.Insert(new TestEntity {Name = "Test"});
+            }
+
+            using (var context = new TestContext(options))
+            {
+                Assert.Equal(1, context.Set<TestEntity>().Count());
+                Assert.Equal("Test", context.Set<TestEntity>().Single().Name);
+            }
+        }
+
+        [Fact]
+        public async void Can_insert_entity_async()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_insert_entity_async")
+                .Options;
+
+            using (var context = new TestContext(options))
+            {
+                var repository = new EfRepository<TestEntity>(context);
+                await repository.InsertAsync(new TestEntity {Name = "Test"});
+            }
+
+            using (var context = new TestContext(options))
+            {
+                Assert.Equal(1, context.Set<TestEntity>().Count());
+                Assert.Equal("Test", context.Set<TestEntity>().Single().Name);
+            }
+        }
+
+        [Fact]
+        public void Can_update_entity()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_update_entity")
+                .Options;
+
+            using (var context = new TestContext(options))
+            {
+                context.Set<TestEntity>().Add(new TestEntity {Name = "Test"});
+                context.SaveChanges();
+            }
+
+            using (var context = new TestContext(options))
+            {
+                var entity = context.Set<TestEntity>().Single();
+
+                entity.Name = "Test (Updated)";
+
+                var repository = new EfRepository<TestEntity>(context);
+                repository.Update(entity);
+            }
+
+            using (var context = new TestContext(options))
+            {
+                Assert.Equal(1, context.Set<TestEntity>().Count());
+                Assert.Equal("Test (Updated)", context.Set<TestEntity>().Single().Name);
+            }
+        }
+
+        [Fact]
+        public async void Can_update_entity_async()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_update_entity_async")
+                .Options;
+
+            using (var context = new TestContext(options))
+            {
+                context.Set<TestEntity>().Add(new TestEntity {Name = "Test"});
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new TestContext(options))
+            {
+                var entity = context.Set<TestEntity>().Single();
+
+                entity.Name = "Test (Updated)";
+
+                var repository = new EfRepository<TestEntity>(context);
+                await repository.UpdateAsync(entity);
+            }
+
+            using (var context = new TestContext(options))
+            {
+                Assert.Equal(1, context.Set<TestEntity>().Count());
+                Assert.Equal("Test (Updated)", context.Set<TestEntity>().Single().Name);
+            }
+        }
+
+        [Fact]
+        public void Can_delete_entity()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_delete_entity")
+                .Options;
+
+            using (var context = new TestContext(options))
+            {
+                context.Set<TestEntity>().Add(new TestEntity {Name = "Test"});
+                context.SaveChanges();
+            }
+
+            using (var context = new TestContext(options))
+            {
+                var entity = context.Set<TestEntity>().Single();
+
+                var repository = new EfRepository<TestEntity>(context);
+                repository.Delete(entity);
+            }
+
+            using (var context = new TestContext(options))
+            {
+                Assert.Equal(0, context.Set<TestEntity>().Count());
+            }
+        }
+
+        [Fact]
+        public async void Can_delete_entity_async()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_delete_entity_async")
+                .Options;
+
+            using (var context = new TestContext(options))
+            {
+                context.Set<TestEntity>().Add(new TestEntity {Name = "Test"});
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new TestContext(options))
+            {
+                var entity = context.Set<TestEntity>().Single();
+
+                var repository = new EfRepository<TestEntity>(context);
+                await repository.DeleteAsync(entity);
+            }
+
+            using (var context = new TestContext(options))
+            {
+                Assert.Equal(0, context.Set<TestEntity>().Count());
+            }
+        }
+
+        [Fact]
+        public void Can_query_dbset()
+        {
+            var options = new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Can_query_dbset")
+                .Options;
+
+            using (var context = new TestContext(options))
+            {
+                context.Set<TestEntity>().AddRange(
+                    new TestEntity {Name = "Test 1"},
+                    new TestEntity {Name = "Test 2"}
+                );
+                context.SaveChanges();
+            }
+
+            using (var context = new TestContext(options))
+            {
+                var repository = new EfRepository<TestEntity>(context);
+                var entities = repository.Table.ToList();
+
+                Assert.NotNull(entities);
+                Assert.Equal(2, entities.Count);
+                Assert.All(entities, entity => Assert.StartsWith("Test", entity.Name));
+            }
+        }
+
+        public class TestContext : DbContext
+        {
+            public TestContext(DbContextOptions options) : base(options)
+            {
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.ApplyConfiguration(new TestEntityMap());
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+
+        public class TestEntity : IEntity
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class TestEntityMap : IEntityTypeConfiguration<TestEntity>
+        {
+            public void Configure(EntityTypeBuilder<TestEntity> builder)
+            {
+                builder.HasKey(e => e.Id);
+                builder.Property(e => e.Name).IsRequired();
+            }
+        }
+    }
+}
