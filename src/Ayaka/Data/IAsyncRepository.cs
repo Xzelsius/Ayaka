3d82cb4c4f05ad1d2f95f4ec64aedf0a86d223f3
Raphael Strotz <Xzelsius@users.noreply.github.com>
2018-06-03 16:32:46 +0200

    Added interfaces for repository pattern

---
+++ src/Ayaka/Data/IAsyncRepository.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Raphael Strotz and other contributors under the MIT license. All rights reserved.
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ayaka.Data
+{
+    /// <summary>
+    ///     Defines functionality to asynchronously interact with data source objects.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    public interface IAsyncRepository<TEntity> where TEntity : IEntity
+    {
+        /// <summary>
+        ///     Provides functionality to evaluate queries against the underlying data source.
+        /// </summary>
+        /// <value>
+        ///     The <see cref="IQueryable{T}" /> to evaluate queries against the underlying data source.
+        /// </value>
+        IQueryable<TEntity> Table { get; }
+
+        /// <summary>
+        ///     Finds an entity by its primary key asynchronously.
+        /// </summary>
+        /// <param name="id">The identifier of the entity.</param>
+        /// <returns>The <typeparamref name="TEntity" /> found for the specified primary key.</returns>
+        Task<TEntity> FindAsync(int id);
+
+        /// <summary>
+        ///     Inserts the <typeparamref name="TEntity" /> into the data source asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to insert.</param>
+        Task InsertAsync(TEntity entity);
+
+        /// <summary>
+        ///     Persists the modification on the <typeparamref name="TEntity" /> to the data source asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        Task UpdateAsync(TEntity entity);
+
+        /// <summary>
+        ///     Deletes the entity from the data source asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        Task DeleteAsync(TEntity entity);
+    }
+}
---
+++ src/Ayaka/Data/IIdentifiable.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Raphael Strotz and other contributors under the MIT license. All rights reserved.
+
+namespace Ayaka.Data
+{
+    /// <summary>
+    ///     Defines functionality of an identifiable object.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the identifier.</typeparam>
+    public interface IIdentifiable<TKey>
+    {
+        /// <summary>
+        ///     Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        ///     A <see cref="TKey" /> value representing the identifier.
+        /// </value>
+        TKey Id { get; set; }
+    }
+}
