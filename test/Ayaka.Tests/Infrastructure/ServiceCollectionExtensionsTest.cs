22d23df092288d21e62f72807f5114fddb5bc590
Raphael Strotz <Xzelsius@users.noreply.github.com>
2018-06-22 20:17:27 +0200

    Updated unit tests

--- test/Ayaka.Tests/Infrastructure/ServiceCollectionExtensionsTest.cs
+++ test/Ayaka.Tests/Infrastructure/ServiceCollectionExtensionsTest.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Raphael Strotz and other contributors under the MIT license. All rights reserved.
 
+using System;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Xunit;
 
 namespace Ayaka.Infrastructure
@@ -12,8 +15,9 @@ namespace Ayaka.Infrastructure
         public void Can_register_services_within_appdomain()
         {
             var serviceCollection = new ServiceCollection();
+            var configuration = new Mock<IConfiguration>();
 
-            serviceCollection.Scan();
+            serviceCollection.Scan(configuration.Object);
 
             Assert.Equal(3, serviceCollection.Count);
             Assert.All(serviceCollection, sc => Assert.Equal(ServiceLifetime.Singleton, sc.Lifetime));
@@ -23,8 +27,9 @@ namespace Ayaka.Infrastructure
         public void Can_register_services_from_assembly()
         {
             var serviceCollection = new ServiceCollection();
+            var configuration = new Mock<IConfiguration>();
 
-            serviceCollection.Scan(typeof(ServiceCollectionExtensionsTest).Assembly);
+            serviceCollection.Scan(configuration.Object, typeof(ServiceCollectionExtensionsTest).Assembly);
 
             Assert.Equal(3, serviceCollection.Count);
             Assert.All(serviceCollection, sc => Assert.Equal(ServiceLifetime.Singleton, sc.Lifetime));
@@ -34,18 +39,27 @@ namespace Ayaka.Infrastructure
         public void Does_respect_call_order()
         {
             var serviceCollection = new ServiceCollection();
+            var configuration = new Mock<IConfiguration>();
 
-            serviceCollection.Scan();
+            serviceCollection.Scan(configuration.Object);
 
             Assert.Equal(3, serviceCollection.Count);
             Assert.Equal(typeof(IService3), serviceCollection.First().ServiceType);
+        }
+
+        [Fact]
+        public void Passes_configuration_to_registrars()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            Assert.Throws<NullReferenceException>(() => serviceCollection.Scan(null));
         }
 
         public class ServiceRegistrar1 : IServiceRegistrar
         {
             public int Order => 1;
 
-            public void ConfigureServices(IServiceCollection services)
+            public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
             {
                 services.AddSingleton<IService1, Service1>();
                 services.AddSingleton<IService2, Service2>();
@@ -56,9 +70,11 @@ namespace Ayaka.Infrastructure
         {
             public int Order => 0;
 
-            public void ConfigureServices(IServiceCollection services)
+            public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
             {
                 services.AddSingleton<IService3, Service3>();
+
+                configuration["Foo"] = "Bar";
             }
         }
 
