d747924819bdc2dcf6ebb72332f23fbb2cf8b40a
Raphael Strotz <Xzelsius@users.noreply.github.com>
2024-07-19 23:20:07 +0200

    chore: add eslint and format workspace

--- docs/.vitepress/shared.ts
+++ docs/.vitepress/shared.ts
@@ -1,26 +1,28 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress';
 
 export const shared = defineConfig({
-    title: "Ayaka",
+  title: 'Ayaka',
 
-    rewrites: {
-        'en/:rest*': ':rest*'
+  base: '/ayaka/',
+
+  rewrites: {
+    'en/:rest*': ':rest*',
+  },
+
+  lastUpdated: true,
+  cleanUrls: true,
+  metaChunk: true,
+
+  themeConfig: {
+    logo: {
+      dark: '/logo-white.svg',
+      light: '/logo-black.svg',
+      width: 24,
+      height: 24,
     },
 
-    lastUpdated: true,
-    cleanUrls: true,
-    metaChunk: true,
-
-    themeConfig: {
-        logo: {
-            dark: "/logo-white.svg",
-            light: "/logo-black.svg",
-            width: 24,
-            height: 24
-        },
-
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-        ]
-    }
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
 });
