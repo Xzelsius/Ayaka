6a96ad0d0c937c1ae0477c9291149c570726e547
Raphael Strotz <Xzelsius@users.noreply.github.com>
2024-11-19 22:01:32 +0100

    feat: add multi-tenancy builder
    Inspired by how Microsoft does it in ASP.NET Core. It's
    the main entry point on configuring multi-tenancy services.

--- src/Ayaka.MultiTenancy/ServiceCollectionExtensions.cs
+++ src/Ayaka.MultiTenancy/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 namespace Ayaka.MultiTenancy;
 
+using Ayaka.MultiTenancy.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -11,6 +12,22 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
+    ///     Adds multi-tenancy services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <remarks>
+    ///     In order to fine-tune the multi-tenancy configuration, use the <see cref="IMultiTenancyBuilder"/> returned by
+    ///     this method.
+    /// </remarks>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+    /// <returns>A <see cref="IMultiTenancyBuilder"/> that can be used to further configure multi-tenancy.</returns>
+    public static IMultiTenancyBuilder AddMultiTenancy(this IServiceCollection services)
+    {
+        ConfigureDefaultServices(services);
+
+        return new MultiTenancyBuilder(services);
+    }
+
+    /// <summary>
     ///     Adds the default implementation for the <see cref="ITenantContextAccessor"/> service.
     /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
@@ -19,5 +36,12 @@ public static class ServiceCollectionExtensions
     {
         services.TryAddSingleton<ITenantContextAccessor, AsyncLocalTenantContextAccessor>();
         return services;
+    }
+
+    [SuppressMessage("Style", "IDE0058:Expression value is never used")]
+    private static void ConfigureDefaultServices(this IServiceCollection services)
+    {
+        // The heart of multi-tenancy
+        services.AddTenantContextAccessor();
     }
 }
--- test/Ayaka.MultiTenancy.Tests/ServiceCollectionExtensionsTest.cs
+++ test/Ayaka.MultiTenancy.Tests/ServiceCollectionExtensionsTest.cs
@@ -2,10 +2,57 @@
 
 namespace Ayaka.MultiTenancy.Tests;
 
+using Ayaka.MultiTenancy.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
 public sealed class ServiceCollectionExtensionsTest
 {
+    public sealed class AddMultiTenancy
+    {
+        [Fact]
+        public void Does_return_instance_of_MultiTenancyBuilder()
+        {
+            var services = new ServiceCollection();
+
+            var builder = services.AddMultiTenancy();
+
+            builder.Should().NotBeNull();
+            builder.Should().BeOfType<MultiTenancyBuilder>();
+        }
+
+        [Fact]
+        public void Does_use_specified_service_collection()
+        {
+            var services = new ServiceCollection();
+
+            var builder = services.AddMultiTenancy();
+
+            builder.Services.Should().BeSameAs(services);
+        }
+
+        [Fact]
+        public void Does_add_default_services()
+        {
+            var services = new ServiceCollection();
+
+            services.AddMultiTenancy();
+
+            var accessor = services.FirstOrDefault(x => x.ServiceType == typeof(ITenantContextAccessor));
+            accessor.Should().NotBeNull("ITenantContextAccessor should be registered");
+        }
+
+        [Fact]
+        public void Does_not_add_default_services_twice()
+        {
+            var services = new ServiceCollection();
+
+            services.AddMultiTenancy();
+            services.AddMultiTenancy();
+
+            services.Count(x => x.ServiceType == typeof(ITenantContextAccessor)).Should().Be(1);
+        }
+    }
+
     public sealed class AddTenantContextAccessor
     {
         [Fact]
