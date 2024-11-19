65c0fd2ef5e73812334465c490e92d263b48971d
Raphael Strotz <17022099+Xzelsius@users.noreply.github.com>
2024-11-19 22:08:39 +0100

    feat: add multi-tenancy builder (#135)
    Co-authored-by: Raphael Strotz <Xzelsius@users.noreply.github.com>

---
+++ src/Ayaka.MultiTenancy.Abstractions/DependencyInjection/IMultiTenancyBuilder.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Raphael Strotz. All rights reserved.
+
+namespace Ayaka.MultiTenancy.DependencyInjection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Provides functionality to configure multi-tenancy services.
+/// </summary>
+public interface IMultiTenancyBuilder
+{
+    /// <summary>
+    /// Gets the <see cref="IServiceCollection"/> where multi-tenancy services are configured.
+    /// </summary>
+    IServiceCollection Services { get; }
+}
---
+++ src/Ayaka.MultiTenancy/DependencyInjection/MultiTenancyBuilder.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Raphael Strotz. All rights reserved.
+
+namespace Ayaka.MultiTenancy.DependencyInjection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Allows configuration of multi-tenancy services.
+/// </summary>
+internal sealed class MultiTenancyBuilder : IMultiTenancyBuilder
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultiTenancyBuilder"/> class.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+    public MultiTenancyBuilder(IServiceCollection services)
+    {
+        Services = services;
+    }
+
+    /// <inheritdoc />
+    public IServiceCollection Services { get; }
+}
