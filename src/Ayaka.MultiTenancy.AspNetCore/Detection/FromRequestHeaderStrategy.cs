c7bf1bdb4a2b23a292f13a27b42637aa69101c34
Raphael Strotz <Xzelsius@users.noreply.github.com>
2025-02-04 22:07:07 +0100

    feat: add request header strategy

---
+++ src/Ayaka.MultiTenancy.AspNetCore/Detection/FromRequestHeaderStrategy.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Raphael Strotz. All rights reserved.
+
+namespace Ayaka.MultiTenancy.AspNetCore.Detection;
+
+using Microsoft.AspNetCore.Http;
+
+/// <summary>
+///     Represents a strategy for detecting the tenant from a request header.
+/// </summary>
+public sealed class FromRequestHeaderStrategy : ITenantDetectionStrategy
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="FromRequestHeaderStrategy"/> class.
+    /// </summary>
+    /// <param name="headerName">The name of the requests header to read the tenant from.</param>
+    public FromRequestHeaderStrategy(string headerName)
+    {
+        HeaderName = headerName;
+    }
+
+    /// <summary>
+    ///     Gets the name of the requests header to read the tenant from.
+    /// </summary>
+    public string HeaderName { get; }
+
+    /// <inheritdoc />
+    public Task<string?> TryDetectAsync(HttpContext context)
+    {
+        var headers = context.Request.Headers;
+
+        if (!headers.TryGetValue(HeaderName, out var headerValues))
+        {
+            return Task.FromResult<string?>(null);
+        }
+
+        string? headerValue;
+
+        // Catch possible multiple header values
+        if (headerValues.Count > 1)
+        {
+            // Log.MultipleHeaderValuesFound(_logger, _headerName);
+            headerValue = headerValues[0];
+        }
+        else
+        {
+            headerValue = headerValues;
+        }
+
+        // Catch possible empty or whitespace value
+        if (string.IsNullOrWhiteSpace(headerValue))
+        {
+            return Task.FromResult<string?>(null);
+        }
+
+        var tenantId = headerValue;
+
+        return Task.FromResult<string?>(tenantId);
+    }
+}
---
+++ test/Ayaka.MultiTenancy.AspNetCore.Tests/Detection/FromRequestHeaderStrategyTest.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Raphael Strotz. All rights reserved.
+
+namespace Ayaka.MultiTenancy.AspNetCore.Tests.Detection;
+
+using Ayaka.MultiTenancy.AspNetCore.Detection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+public sealed class FromRequestHeaderStrategyTest
+{
+    [Fact]
+    public void Getting_HeaderName_returns_the_header_name()
+    {
+        var actual = new FromRequestHeaderStrategy("X-Tenant-Id");
+
+        actual.HeaderName.Should().Be("X-Tenant-Id");
+    }
+
+    [Fact]
+    public async Task Does_return_null_when_no_header_is_available()
+    {
+        var httpRequest = A.Fake<HttpRequest>();
+        A.CallTo(() => httpRequest.Headers).Returns(new HeaderDictionary());
+
+        var httpContext = A.Fake<HttpContext>();
+        A.CallTo(() => httpContext.Request).Returns(httpRequest);
+
+        var actual = await new FromRequestHeaderStrategy("X-Tenant-Id").TryDetectAsync(httpContext);
+
+        actual.Should().BeNull();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task Does_return_null_when_header_has_empty_value(string? headerValue)
+    {
+        var httpRequest = A.Fake<HttpRequest>();
+        A.CallTo(() => httpRequest.Headers)
+            .Returns(
+                new HeaderDictionary(
+                    new Dictionary<string, StringValues>
+                    {
+                        { "X-Tenant-Id", headerValue }
+                    }));
+
+        var httpContext = A.Fake<HttpContext>();
+        A.CallTo(() => httpContext.Request).Returns(httpRequest);
+
+        var actual = await new FromRequestHeaderStrategy("X-Tenant-Id").TryDetectAsync(httpContext);
+
+        actual.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task Does_return_header_value_as_tenant_identifier()
+    {
+        var httpRequest = A.Fake<HttpRequest>();
+        A.CallTo(() => httpRequest.Headers)
+            .Returns(
+                new HeaderDictionary(
+                    new Dictionary<string, StringValues>
+                    {
+                        { "X-Tenant-Id", "example" }
+                    }));
+
+        var httpContext = A.Fake<HttpContext>();
+        A.CallTo(() => httpContext.Request).Returns(httpRequest);
+
+        var actual = await new FromRequestHeaderStrategy("X-Tenant-Id").TryDetectAsync(httpContext);
+
+        actual.Should().Be("example");
+    }
+
+    [Fact]
+    public async Task Does_return_first_header_value_as_tenant_identifier_when_multiple_values_are_available()
+    {
+        var httpRequest = A.Fake<HttpRequest>();
+        A.CallTo(() => httpRequest.Headers)
+            .Returns(
+                new HeaderDictionary(
+                    new Dictionary<string, StringValues>
+                    {
+                        { "X-Tenant-Id", new StringValues(["example", "example2"]) }
+                    }));
+
+        var httpContext = A.Fake<HttpContext>();
+        A.CallTo(() => httpContext.Request).Returns(httpRequest);
+
+        var actual = await new FromRequestHeaderStrategy("X-Tenant-Id").TryDetectAsync(httpContext);
+
+        actual.Should().Be("example");
+    }
+}
