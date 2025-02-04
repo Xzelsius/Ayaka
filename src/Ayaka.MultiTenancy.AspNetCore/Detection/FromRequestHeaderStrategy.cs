0791c20008118a3746ae4aa83c4c374722ababde
Raphael Strotz <Xzelsius@users.noreply.github.com>
2025-02-04 22:07:07 +0100

    feat: add request header strategy

---
+++ src/Ayaka.MultiTenancy.AspNetCore/Detection/FromRequestHeaderStrategy.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Raphael Strotz. All rights reserved.
+
+namespace Ayaka.MultiTenancy.AspNetCore.Detection;
+
+using Microsoft.AspNetCore.Http;
+
+/// <summary>
+///     Represents a strategy for detecting the tenant from a request header.
+/// </summary>
+public class FromRequestHeaderStrategy : ITenantDetectionStrategy
+{
+    private readonly string _headerName;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="FromRequestHeaderStrategy"/> class.
+    /// </summary>
+    /// <param name="headerName">The name of the requests header to read the tenant from.</param>
+    public FromRequestHeaderStrategy(string headerName)
+    {
+        _headerName = headerName;
+    }
+
+    /// <inheritdoc />
+    public Task<string?> TryDetectAsync(HttpContext context)
+    {
+        var headers = context.Request.Headers;
+
+        if (!headers.TryGetValue(_headerName, out var headerValues))
+        {
+            return Task.FromResult<string?>(null);
+        }
+
+        string? headerValue;
+
+        // Catch possible multiple header values
+        if (headerValues.Count > 1)
+        {
+            // Log.MultipleHeaderValuesFound(_logger, _headerName);
+            headerValue = headerValues[0];
+        }
+        else
+        {
+            headerValue = headerValues;
+        }
+
+        // Catch possible empty or whitespace value
+        if (string.IsNullOrWhiteSpace(headerValue))
+        {
+            return Task.FromResult<string?>(null);
+        }
+
+        var tenantId = headerValue;
+
+        return Task.FromResult<string?>(tenantId);
+    }
+}
