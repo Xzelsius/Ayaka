2fb6b868ccf4f3b33bf7e0926b285bcc110a22d9
Raphael Strotz <Xzelsius@users.noreply.github.com>
2025-03-03 17:03:08 +0100

    feat: add empty builder for RequestTenancyOptions

---
+++ src/Ayaka.MultiTenancy.AspNetCore/DependencyInjection/MultiTenancyBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Raphael Strotz. All rights reserved.
+
+namespace Ayaka.MultiTenancy.DependencyInjection;
+
+using Ayaka.MultiTenancy.AspNetCore;
+
+/// <summary>
+///     Extension methods for <see cref="IMultiTenancyBuilder" />.
+/// </summary>
+public static class MultiTenancyBuilderExtensions
+{
+    /// <summary>
+    ///     Configures the <see cref="RequestTenancyMiddleware"/> middleware.
+    /// </summary>
+    /// <param name="builder">The <see cref="IMultiTenancyBuilder"/> to configure the request tenancy on.</param>
+    /// <param name="configure">A delegate to configure the <see cref="RequestTenancyOptions"/>.</param>
+    /// <returns>The same <see cref="IMultiTenancyBuilder"/>.</returns>
+    public static IMultiTenancyBuilder ConfigureRequestTenancy(this IMultiTenancyBuilder builder, Action<RequestTenancyBuilder> configure)
+    {
+        var requestTenancyBuilder = new RequestTenancyBuilder(builder);
+        configure(requestTenancyBuilder);
+
+        requestTenancyBuilder.ConfigureServices();
+
+        return builder;
+    }
+}
