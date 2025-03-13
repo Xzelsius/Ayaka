122a674e32e2e4658f745c98bd03c8c5d9632a84
Raphael Strotz <Xzelsius@users.noreply.github.com>
2025-03-13 21:21:03 +0100

    feat: introduce extension that allows to use .When() as before

--- src/Ayaka.Nuke/Extensions.cs
+++ src/Ayaka.Nuke/Extensions.cs
@@ -8,6 +8,17 @@ namespace Ayaka.Nuke;
 public static class Extensions
 {
     /// <summary>
+    ///     Invokes the <paramref name="configurator" /> if the <paramref name="condition" /> is <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to operate on.</typeparam>
+    /// <param name="settings">The object to operate on.</param>
+    /// <param name="condition">The condition to check.</param>
+    /// <param name="configurator">The method to execute if <paramref name="condition" /> is <c>true</c>.</param>
+    /// <returns>The same <paramref name="settings" /> instance for chaining.</returns>
+    public static T When<T>(this T settings, bool condition, Func<T, T> configurator)
+        => condition ? configurator.Invoke(settings) : settings;
+
+    /// <summary>
     ///     Invokes the <paramref name="configurator" /> if the <paramref name="obj" /> is not <c>null</c>.
     /// </summary>
     /// <typeparam name="T">The type of the object to operate on.</typeparam>
@@ -16,6 +27,6 @@ public static class Extensions
     /// <param name="obj">The object to check for <c>null</c>.</param>
     /// <param name="configurator">The method to execute if <paramref name="obj" /> is not <c>null</c>.</param>
     /// <returns>The same <paramref name="settings" /> instance for chaining.</returns>
-    public static T WhenNotNull<T, TObject>(this T settings, TObject? obj, Func<T, TObject, T> configurator) =>
-        obj != null ? configurator.Invoke(settings, obj) : settings;
+    public static T WhenNotNull<T, TObject>(this T settings, TObject? obj, Func<T, TObject, T> configurator)
+        => obj != null ? configurator.Invoke(settings, obj) : settings;
 }
